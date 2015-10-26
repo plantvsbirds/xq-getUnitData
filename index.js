f331c4b132a57578c812c66f66c8c303f5457c56
Hong Lin <plantvsbird@gmail.com>
2015-10-26 16:55:40 +0800

    Now with simplfied API, no `scrap` method anymore

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var phantom = require('phantom')
 
 var prettyjson = require('prettyjson')
 var log = (obj) => console.log(prettyjson.render(obj, {
-  
+
 }))
 var write = (obj) => process.stdout.write(JSON.stringify(obj))
 
 var rebalanceUrl = (unitId, count) =>
-  'http://xueqiu.com/cubes/rebalancing/history.json?cube_symbol='+ unitId +'&count='+ count +'&page=1'
+  'http://xueqiu.com/cubes/rebalancing/history.json?cube_symbol=' + unitId + '&count=' + count + '&page=1'
 
 function ScraperForUnit(userDefinedSettings) {
   this.settings = {
@@ -18,71 +18,69 @@ function ScraperForUnit(userDefinedSettings) {
     if (userDefinedSettings[k])
       this.settings[k] = userDefinedSettings[k]
   })
-}
+  return (unitId) =>
+    new Promise((resolve, reject) => {
+      var self = this
+      phantom.create(function(ph) {
+        ph.createPage(function(page) {
 
-ScraperForUnit.prototype.scrap = (unitId) =>
-  new Promise((resolve, reject) => {
-  var self = this
-  phantom.create(function(ph) {
-      ph.createPage(function(page) {
+          page.set('settings.userAgent', 'Mozilla/5.0 (Windows NT 6.1 WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')
 
-        page.set('settings.userAgent', 'Mozilla/5.0 (Windows NT 6.1 WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')
+          page.set('onResourceReceived',
+            function(requestData, request) {
+              var log = function(obj) {
+                console.log(JSON.stringify(obj))
+              }
+              if (requestData.url.indexOf('.json') === -1)
+                return
+                //write('[JSONPACK BOOM]')
+            });
 
-        page.set('onResourceReceived',
-          function(requestData, request) {
-            var log = function(obj) {
-              console.log(JSON.stringify(obj))
-            }
-            if (requestData.url.indexOf('.json') === -1)
-              return
-            //write('[JSONPACK BOOM]')
-          });
-
-        var replay = require('./replay')(page, self.settings)
-        var getMeta = require('./unit-data.js')(page, self.settings)
-        page.open("http://xueqiu.com/p/" + unitId, function(status) {
-          Promise.all([
-            replay(rebalanceUrl(unitId, 1), '')
-              .then((firstButch) => replay(rebalanceUrl(unitId, firstButch.data.totalCount), 'rebalance_history'))
-            , getMeta()
-           ])
-          .then((data) => {
-              log(data)
-              let ans = {}
-              data.forEach((item) => {
-                ans[item.tag] = item.data
-              })
-              resolve(ans)
-            }).catch(reject)
+          var replay = require('./replay')(page, self.settings)
+          var getMeta = require('./unit-data.js')(page, self.settings)
+          page.open("http://xueqiu.com/p/" + unitId, function(status) {
+            Promise.all([
+              replay(rebalanceUrl(unitId, 1), '')
+              .then((firstButch) => replay(rebalanceUrl(unitId, firstButch.data.totalCount), 'rebalance_history')), getMeta()
+            ])
+              .then((data) => {
+                log(data)
+                let ans = {}
+                data.forEach((item) => {
+                  ans[item.tag] = item.data
+                })
+                resolve(ans)
+              }).catch(reject)
           })
         })
       })
 
     })
+}
 
-    module.exports = (settings) => new ScraperForUnit(settings)
-
-
-    var selfExec = () => {
-      log('Not called as a module. Getting data from default')
-      module.exports({
-
-      }).scrap('ZH087953')
-    }
-
-    if (!module.parent) {
-      selfExec()
-    }
-
-    /* TODO
-     * retry logic
-     * module solution : just return data as JSON(BSON-compatible)
-     * beanstalk
-     */
+module.exports = (settings) => new ScraperForUnit(settings)
 
 
-    /*
-     * module api:
-     *  logging : open, close, indent, color output
-     *  beanstalk watch
-     */
+var selfExec = () => {
+  log('Not called as a module. Getting data from default')
+  module.exports({
+
+  })('ZH087953').then(() => null)
+}
+
+if (!module.parent) {
+  selfExec()
+}
+
+/* TODO
+ * retry logic
+ * module solution : just return data as JSON(BSON-compatible)
+ * beanstalk
+ */
+
+
+/*
+ * module api:
+ *  logging : open, close, indent, color output
+ *  beanstalk watch
+ */
