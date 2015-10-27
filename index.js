0d33828af830589f559c8b22f576df68fb499feb
Hong Lin <plantvsbird@gmail.com>
2015-10-28 00:28:20 +0800

    Now with a setting disabling images for higher performance

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ function ScraperForUnit(userDefinedSettings) {
         ph.createPage(function(page) {
 
           page.set('settings.userAgent', 'Mozilla/5.0 (Windows NT 6.1 WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36')
+          page.set('settings.loadImages', false)
 
           page.set('onResourceReceived',
             function(requestData, request) {
